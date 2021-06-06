409e7e0202d15c66280d9e31fb225ad7aaf24c7e
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-06-06 10:21:50 -0300

    feat configurando index

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const createError = require('http-errors') // tratativas de erros.
 const express = require('express')
+const app = express()
+const logger = require('morgan')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.get('/', (req, res) => res.send("Nada por aqui :)"))
+
+app.use((req, res, next) => {
+    next(createError(404))
+})
+
+app.use((err, req, res) => {
+    res.status(err.status || 500) //mensagem de erro.
+    res.json(err)
+})
+
+module.exports = app;
