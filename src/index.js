6c2210eed717797bc1d64716f463b082cdf350ea
Leonardo Santos Gonçales <leondani853@gmail.com>
2021-06-06 10:27:56 -0300

    chore criando pasta src

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const logger = require('morgan')
 
+app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.get('/', (req, res) => res.send("Nada por aqui :)"))
